722dd1a4a3b4e8f0a2bd8c7452d2359947f1b9af
Gwen <g@10degres.net>
2023-02-28 10:52:45 +0100

    index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ Silence is golden.
 <!-- subdomain takeover PoC by glc -->
 <?php var_dump($_GET); ?>
 <?php var_dump($_COOKIES); ?>
+<a href="javascript:alert(document.cookie);">ccc</a>
 <?php phpinfo(); ?>
 </body>
 <html>
