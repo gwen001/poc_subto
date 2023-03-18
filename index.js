ba4d6b9c9950ee2e2bd2476b62347a3708c9ab7b
Gwen <g@10degres.net>
2023-03-18 11:34:19 +0100

    indexes

---
+++ index.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+Silence is golden.
+<!-- subdomain takeover PoC by glc -->
+<br><br>
+<a href="javascript:alert(document.cookie);">click here to view your cookies</a>
+</body>
+<html>
