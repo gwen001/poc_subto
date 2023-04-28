326a376112fab6b86f5b4e5721b9a9763687bd93
Gwendal Le Coguic <gwen001@users.noreply.github.com>
2023-04-28 11:16:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 Silence is golden.
 <!-- subdomain takeover PoC by glc -->
 <br><br>
-<a href="javascript:alert(document.cookie);">click here to view your cookies</a>
+<a href="javascript:alert(document.cookie);">click here to view your cookies with JS</a><br><br>
+Good read:
+<ul>
+<li>https://labs.detectify.com/2014/10/21/hostile-subdomain-takeover-using-herokugithubdesk-more/
+<li>https://0xpatrik.com/subdomain-takeover/
+<li>https://www.hackerone.com/application-security/guide-subdomain-takeovers
+</ul>
 </body>
 <html>
