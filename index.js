56e62fe97a296cb8954211d27a0ca543728c602c
Gwen <g@10degres.net>
2023-09-30 12:08:18 +0200

    links

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ Silence is golden.
 <a href="javascript:alert(document.cookie);">click here to view your cookies with JS</a><br><br>
 Good read:
 <ul>
-<li>https://labs.detectify.com/2014/10/21/hostile-subdomain-takeover-using-herokugithubdesk-more/
-<li>https://0xpatrik.com/subdomain-takeover/
-<li>https://www.hackerone.com/application-security/guide-subdomain-takeovers
+<li><a href="https://labs.detectify.com/2014/10/21/hostile-subdomain-takeover-using-herokugithubdesk-more/" target="_blank">Hostile Subdomain Takeover using Heroku/Github/Desk + more</a></li>
+<li><a href="https://www.honeybadger.io/blog/subdomain-takeover/" target="_blank">Subdomain Takeover: Ignore This Vulnerability at Your Peril</a></li>
+<li><a href="https://0xpatrik.com/subdomain-takeover/" target="_blank">Subdomain Takeover: Thoughts on Risks</a></li>
+<li><a href="https://www.hackerone.com/application-security/guide-subdomain-takeovers" target="_blank">A Guide To Subdomain Takeovers</a></li>
 </ul>
 </body>
 <html>
