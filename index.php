0a77814f9be14fa11c167ef991df907f49f1e274
Gwen <g@glc.st>
2026-09-15 12:38:15 +0200

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 Silence is golden and subdomain takeover still an issue 🤠​
-<!-- subdomain takeover PoC by glc: <?php echo $_SERVER['SERVER_NAME']; ?> -->
+<!-- subdomain takeover PoC by https://glc.st -->
 <br><br>
 <a href="javascript:alert(document.cookie);">click here to view your cookies with JS</a><br><br>
 Good read:
