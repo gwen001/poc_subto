8e99bb03e80f4a819cd6673352fb9176e852172d
Gwen <g@glc.st>
2026-09-17 21:55:27 +0200

    test

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+Silence is golden and subdomain takeover still an issue ​🤖​​
+<!-- subdomain takeover PoC by https://glc.st -->
+<br><br>
+<a href="javascript:alert(document.cookie);">click here to view your cookies with JS</a><br><br>
+Good read:
+<ul>
+<li><a href="https://labs.detectify.com/2014/10/21/hostile-subdomain-takeover-using-herokugithubdesk-more/" target="_blank">Hostile Subdomain Takeover using Heroku/Github/Desk + more</a></li>
+<li><a href="https://www.honeybadger.io/blog/subdomain-takeover/" target="_blank">Subdomain Takeover: Ignore This Vulnerability at Your Peril</a></li>
+<li><a href="https://0xpatrik.com/subdomain-takeover/" target="_blank">Subdomain Takeover: Thoughts on Risks</a></li>
+<li><a href="https://www.hackerone.com/application-security/guide-subdomain-takeovers" target="_blank">A Guide To Subdomain Takeovers</a></li>
+</ul>
+Below your cookies with PHP:
+<pre>
+<?php
+//phpinfo();
+if( isset($_COOKIE) && count($_COOKIE) ) {
+  var_dump($_COOKIE);
+}
+?>
+</pre>
+</body>
+<html>
